38199d353047f5a2a29d2261a64e2b72fdc439ce
きんぐ <abbamigimigi@gmai.com>
2016-05-07 19:49:54 +0900

    v1.0.3
    Unityちゃん持ってきた。走りモーションしてるけど実際は止まってて足場が動いてる。
    ジャンプ出来ない。カメラは縦固定でキャラ追従にしないといけない。

---
+++ Assets/Scripts/Game/PlayerScript.cs
@@ -0,0 +1,219 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerScript : MonoBehaviour {
+
+    Rigidbody2D rb;
+    Animator anim;
+
+    //ジャンプ関連
+    public LayerMask groundLayer;//地面のレイヤー
+    bool isGrounded; //着地しているかの判定
+    bool isNotSideFloor_Up; //フロア関連
+    private static int restJumps = 2; //ジャンプ回数
+    private float jumpHeight_1 = 1.5f;    //レベルデザインで数値変更よろす
+    private float jumpHeight_2 = 1f;    //レベルデザインで数値変更よろす
+
+    //音関連
+    private AudioSource SE_Jump;
+    private AudioSource SE_Landing;
+    private AudioSource SE_Bom;
+    private AudioSource SE_OneSpike;
+    private AudioSource Jump_01;
+    private AudioSource Jump_02;
+    private AudioSource GameOver002;
+    private AudioSource GameClear002;
+
+    //リザルト関連
+    private GameObject ResultEdit;
+	private bool isCleard = false;
+	private bool isGameOver = false;
+	private bool isScoreSaveEnd = false;
+
+    /* 実機の速度を上げれるかもしれないコード↓
+    void Awake() {
+        QualitySettings.vSyncCount = 1;
+        Application.targetFrameRate = 60;
+    }
+    */
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>(); //GetComponentの処理をキャッシュしておく
+        anim = GetComponent<Animator>(); //Animatorをキャッシュ
+
+        //SEをキャッシュ
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        SE_Jump = audioSources[0];
+        SE_Landing = audioSources[1];
+        SE_Bom = audioSources[2];
+        SE_OneSpike = audioSources[3];
+        Jump_01 = audioSources[4];
+        Jump_02 = audioSources[5];
+    }
+
+    //Updateだとクリックを判定できない時があるため、
+    //ジャンプの処理はUpdateメソッドに記述
+    void Update() {
+        anim.SetBool("Run", true);
+        //if (GlobalVariableScript.moveSpeed == 0 && isGameOver == false) {
+        //anim.speed = 1;
+        //anim.SetTrigger("Stay");
+        // } else if (GlobalVariableScript.moveSpeed == 0 && isGameOver == true){
+
+        // }
+
+        //クリアしたかどうかの判定を追加
+        //終了処理は一度だけ呼ぶ
+        /*
+        if (isScoreSaveEnd == false) {
+			if (GlobalVariableScript.Gamemode == GlobalVariableScript.GamemodeFloor10) {
+				//Floor10のゴール階数に到達した場合クリアフラグを立てる
+				if(GlobalVariableScript.FloorCount == GlobalVariableScript.Floor10Goal) isCleard = true;
+			} else if (GlobalVariableScript.Gamemode == GlobalVariableScript.GamemodeFloor20) {
+				//Floor20のゴール階数に到達した場合クリアフラグを立てる
+				if(GlobalVariableScript.FloorCount == GlobalVariableScript.Floor20Goal) isCleard = true;
+			} else if (GlobalVariableScript.Gamemode == GlobalVariableScript.GamemodeFloor30) {
+				//Floor30のゴール階数に到達した場合クリアフラグを立てる
+				if(GlobalVariableScript.FloorCount == GlobalVariableScript.Floor30Goal) isCleard = true;
+			} 
+
+
+			// クリアフラグが立っていた場合
+			if (isCleard == true) {
+				//anim.speed = 0;                       		// アニメーション停止
+				rb.isKinematic = true;
+				restJumps = 0;                        		// ジャンプ回数0
+				GlobalVariableScript.moveSpeed = 0;   		// 移動速度0
+				GlobalVariableScript.isCountDown = false;	// カウントダウン停止
+
+				//データセーブ処理呼び出し
+				ResultEdit = GameObject.Find ("ResultEdit");
+				ResultEdit.SendMessage ("ResultClear");
+				isScoreSaveEnd = true;
+                
+			}
+		}
+
+        */
+
+        //ユニティちゃん中央から足元にかけて、接地判定用のラインを引く
+        isGrounded = Physics2D.Linecast(
+            transform.position + transform.up * 1,
+            transform.position - transform.up * 0.1f,
+            groundLayer); // Linecastが判定するレイヤー
+
+        //スマートフォン用
+        if (Input.touchCount > 0) {
+            if (GlobalVariableScript.isCountDown == false) {    //カウントダウン時は操作を受け付けない
+                return;
+            }
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began) {
+                if (restJumps > 0) {
+                    if (isGrounded == false) {
+                        restJumps = 1;
+                    }
+                    Jump();                                     //ジャンプメソッドで実行
+                }
+            }
+        }
+
+        //PC版用
+        if (Input.GetButtonDown("Jump")) {
+            if (GlobalVariableScript.isCountDown == false) {    //カウントダウン時は操作を受け付けない
+                return;
+            }
+            if (restJumps > 0) {
+                if (isGrounded == false) {
+                    restJumps = 1;
+                }
+                Jump();                                         //ジャンプメソッドで実行
+            }
+        }
+        Anim();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        //velocity:速度
+        //X方向へmoveSpeed分移動させる
+		rb.velocity = new Vector2(transform.localScale.x * GlobalVariableScript.moveSpeed,
+            rb.velocity.y);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col) { // 2Dの衝突判定
+      
+        if (col.gameObject.tag == "Floor") {
+            restJumps = 2; //ジャンプ回数リセット
+            SE_Landing.PlayOneShot(SE_Landing.clip); // 着地音再生
+        }
+        /*
+        if (col.gameObject.tag == "Spike_Big") {
+            SE_Bom.PlayOneShot(SE_Bom.clip);      // 爆発音再生
+            GameOver();
+            isGameOver = true;						// ゲームオーバーフラグを立てる
+        }
+        if (col.gameObject.tag == "OneSpike") {
+            SE_OneSpike.PlayOneShot(SE_OneSpike.clip);      // 爆発音再生
+            GameOver();
+			isGameOver = true;						// ゲームオーバーフラグを立てる
+        }
+
+		// ゲームオーバーだった場合
+		if (isGameOver == true) {
+			// カウントダウン停止
+			GlobalVariableScript.isCountDown = false;	
+
+			//データ表示処理呼び出し
+			ResultEdit = GameObject.Find ("ResultEdit");
+			ResultEdit.SendMessage ("ResultGameOver");
+
+		}
+        */	
+	}
+    /*
+    private void GameOver() {
+        anim.speed = 0;
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
+        restJumps = 0;                        // ジャンプ回数0
+        GlobalVariableScript.moveSpeed = 0;   // 移動速度0
+        GlobalVariableScript.isGameOver_BGM = true; // BGM再生停止フラグON
+    }
+    */
+    void Jump() {    //ジャンプの処理を全てまとめました
+
+        anim.SetTrigger("Jump");   //ジャンプアニメーションの開始
+
+        SE_Jump.PlayOneShot(SE_Jump.clip);    //ジャンプSEの再生
+
+        if (restJumps == 2) {    //初回ジャンプ時の動作
+            Jump_01.PlayOneShot(Jump_01.clip);    //ジャンプボイスの再生
+            float gravity = Mathf.Abs(Physics.gravity.y);
+            float velocity = Mathf.Sqrt(2 * gravity * jumpHeight_1);
+            this.rb.velocity = Vector3.up * velocity;
+            restJumps--;
+        } else if (restJumps == 1) {    //二段ジャンプ時の動作
+            Jump_02.PlayOneShot(Jump_02.clip);    //ジャンプボイスの再生
+            float gravity = Mathf.Abs(Physics.gravity.y);
+            float velocity = Mathf.Sqrt(2 * gravity * jumpHeight_2);
+            this.rb.velocity = Vector3.up * velocity;
+            restJumps--;
+        }
+        isGrounded = false; //地面から離れるのでfalseにする
+
+    }
+
+    void Anim() {
+        //velY:y方向へ掛かる速度単位、上へ行くとプラス、下へ行くとマイナス
+        float velY = rb.velocity.y;
+        //Animatorへパラメーターを送る
+        anim.SetFloat("velY", velY);
+        anim.SetBool("isGrounded", isGrounded);
+
+    }
+    
+}
---
+++ Assets/Scripts/GlobalVariableScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalVariableScript : MonoBehaviour {
+
+    public const string BGMVolumePrefs = "BGMVolume";   //Music Volumeのデータを書き込むPlayerPrefs
+    public const string SEVolumePrefs = "SEVolume"; //Effect Volumeのデータを書き込むPlayerPrefs
+
+    //プレイヤーのステータス
+    public const int MoveSpeedDefault = 0;      //デフォルトの横移動速度
+
+    public static int moveSpeed = MoveSpeedDefault;         // ユニティちゃんの移動速度
+    public static float timer = 0;                          // 秒数
+    public static bool isCountDown = false;					// カウントダウンするかのフラグ
+    public static bool isGameOver_BGM = false;              // GameOver時、BGMの再生停止のフラグ
+    public static bool isPause_BGM = false;                 // Pause時、BGMの一時停止のフラグ
+
+    // このオブジェクト用の変数
+    private static bool isCreatedObj = false;   // オブジェクト作成済みフラグ
+
+    void Awake()
+    {
+        // 作成済みフラグがoffの場合
+        if (isCreatedObj == false)
+        {
+            // シーンが切り替わってもオブジェクトを破棄しない
+            DontDestroyOnLoad(this.gameObject);
+
+            // 作成済みフラグをOnにする
+            isCreatedObj = true;
+            // 作成済みフラグがonの場合
+        }
+        else {
+            // オブジェクトを破棄する
+            Destroy(this.gameObject);
+        }
+    }
+}
